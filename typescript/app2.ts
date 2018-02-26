61d5ef040deb67c45b5585c23a4aff0270a14b02
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-26 11:06:27 -0300

    Class Carro - Criando e instanciando nossa primeira classe

---
+++ typescript/app2.ts
@@ -0,0 +1,32 @@
+/** 
+ * Classe que representa um carro
+*/
+class Carro {
+
+    private modelo: string;
+    private numeroDePortas: number;
+    private velocidade: number = 0;
+
+    constructor(modelo: string, numeroDePortas: number) {
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+
+    public acelerar(): void {
+        this.velocidade = this.velocidade + 10;
+    }
+
+    public parar(): void {
+        this.velocidade = 0;
+    }
+
+    public velocidadeAtual(): number {
+        return this.velocidade;
+    }
+
+}
+
+let carroA = new Carro("Fiesta", 4);
+console.log(carroA);
+carroA.acelerar();
+console.log(carroA);
---
+++ typescript/dist/app2.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * Classe que representa um carro
+*/
+var Carro = /** @class */ (function () {
+    function Carro(modelo, numeroDePortas) {
+        this.velocidade = 0;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
+    return Carro;
+}());
+var carroA = new Carro("Fiesta", 4);
+console.log(carroA);
+carroA.acelerar();
+console.log(carroA);
