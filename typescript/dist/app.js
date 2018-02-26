b0587409bc4dee2426fc93fa659a71459bfe44d9
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-26 10:19:36 -0300

    Entendendo o compilador e o arquivo de configuração tsconfig.json

--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,11 +1,12 @@
+"use strict";
 /**
- * Type script utilizando arrow function (ES6) e 
+ * Type script utilizando arrow function (ES6) e
  * tipagem de dados (TS).
- * 
+ *
  * Rodando o comando tsc app ele fará o transpile gerando
  * o arquivo app.js com o ES5
  */
-let ola = (nome: string, sobreNome: string) => {
+var ola = function (nome, sobreNome) {
     console.log('Olá ' + nome + ' ' + sobreNome);
-}
+};
 ola('Ricardo', 'Ruiz');
