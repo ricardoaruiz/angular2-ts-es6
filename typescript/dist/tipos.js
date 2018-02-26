1a472ab011e8e883b765eda32258aaf51e57afb3
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-26 10:46:15 -0300

    Inferência de tipos

---
+++ typescript/dist/tipos.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * Podemos definir os tipos das variáveis e caso tentamos
+ * colocar um valor que não seja do tipo termos um problema
+ * de compilação.
+ */
+var mensagem = 'Seja bem vindo!!';
+var temporadaFriends = 10;
+var estudandoAngular = true;
+//Arrays
+var listaDeFrutas1 = ['Uva', 'Banana', 'Abacaxi'];
+var listaDeFrutas2 = ['Uva', 'Banana', 'Abacaxi'];
+var notasDasProvas1 = [7.5, 8, 9];
+var notasDasProvas2 = [5, 6, 7];
---
+++ typescript/tipos.ts
@@ -0,0 +1,15 @@
+/**
+ * Podemos definir os tipos das variáveis e caso tentamos
+ * colocar um valor que não seja do tipo termos um problema
+ * de compilação.
+ */
+let mensagem: string = 'Seja bem vindo!!'
+let temporadaFriends: number = 10
+let estudandoAngular: boolean = true
+
+//Arrays
+let listaDeFrutas1: Array<string> = ['Uva','Banana','Abacaxi']
+let listaDeFrutas2: string[] = ['Uva','Banana','Abacaxi']
+
+let notasDasProvas1: Array<number> = [7.5,8,9]
+let notasDasProvas2: number[] = [5,6,7]
