584f6fa67fdba9f96da9c58890f7890b194da19f
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-27 14:28:38 -0300

    App1 - Criando a classe Coração

---
+++ angular/projetos/app1/src/app/shared/coracao.model.ts
@@ -0,0 +1,12 @@
+export class Coracao {
+
+    constructor(
+        public cheio: boolean,
+        public urlCoracaoCheio: string = '/assets/coracao_cheio.png',
+        public urlCoracaoVazio: string = '/assets/coracao_cheio.png'
+    ) {}
+
+    public exibeCoracao(): string {
+        return this.cheio ? this.urlCoracaoCheio : this.urlCoracaoVazio;
+    }
+}
--- angular/projetos/app1/src/app/tentativas/tentativas.component.ts
+++ angular/projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Coracao } from '../shared/coracao.model';
+
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
 export class TentativasComponent implements OnInit {
+
+  public coracoes: Coracao[] = [
+    new Coracao(true), 
+    new Coracao(true), 
+    new Coracao(true)
+  ];
 
   public coracaoVazio: string = '/assets/coracao_vazio.png';
   public coracaoCheio: string = '/assets/coracao_cheio.png' 
