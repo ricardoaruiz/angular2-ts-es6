62ee8ac5f025abb6633d5e8a6a1375d44b579c4c
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-27 12:54:51 -0300

    App1 - Criando um Mock de objetos Frase

---
+++ angular/projetos/app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+const FRASES:  Frase[] = [
+    new Frase('I like to learn','Eu gosto de aprender'),
+    new Frase('I watch tv','Eu assisto tv'),
+    new Frase('How are you','Como vai você'),
+    new Frase('I eat bread','Eu como pão')
+];
