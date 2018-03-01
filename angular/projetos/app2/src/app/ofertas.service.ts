2d02fa881e9c6d2f695117e1b58d592103f5a071
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-01 14:41:13 -0300

    App2 - Recuperando ofertas de restaurante via HTTP

--- angular/projetos/app2/src/app/ofertas.service.ts
+++ angular/projetos/app2/src/app/ofertas.service.ts
@@ -13,15 +13,35 @@ import { Oferta } from './shared/oferta.model';
 @Injectable()
 export class OfertasService {
 
+    private baseURL: string = 'http://localhost:3000';
+
     constructor(private http: Http) {}
 
+    /** 
+     * Obtem todas as ofertas.
+    */
     public getOfertas(): Promise<Oferta[]> {
 
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(this.baseURL + '/ofertas?destaque=true')
             .toPromise()
                 .then( (resposta: any) => {                    
                     return resposta.json();
                 })
+    }
+
+    /**
+     * Obtém todas as ofertas por categoria
+     * 
+     * @param categoria 
+     */
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+
+        return this.http.get(this.baseURL + `/ofertas?categoria=${categoria}`)
+            .toPromise()
+                .then( (resposta: any) => {
+                    return resposta.json();
+                })
+
     }
 
 }
--- angular/projetos/app2/src/app/restaurantes/restaurantes.component.ts
+++ angular/projetos/app2/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
+
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers: [ OfertasService ]
 })
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor(private ofertasServices: OfertasService) { }
 
   ngOnInit() {
+    this.carregarOfertas();
+  }
+
+  private carregarOfertas(): void {
+    this.ofertasServices.getOfertasPorCategoria('restaurante')
+      .then( (ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+        console.log(this.ofertas);
+      })
   }
 
 }
