423ebcc722909a36d855235ee8ef3393450991c0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-03-04 15:13:37 -0300

    App2 - Capturando a entrada do usuário - Approach: $event e <HTMLInputElement>

--- angular/projetos/app2/src/app/topo/topo.component.ts
+++ angular/projetos/app2/src/app/topo/topo.component.ts
@@ -12,4 +12,8 @@ export class TopoComponent implements OnInit {
   ngOnInit() {
   }
 
+  public capturarFiltro($event: Event) {
+     let filtroInformado = (<HTMLInputElement>$event.target).value
+  }
+
 }
