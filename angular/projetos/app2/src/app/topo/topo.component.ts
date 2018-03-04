110399de679dd0b5a7ea30707325adef9b750603
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-03-04 20:54:03 -0300

    App2-Lógica da pesquisa de ofertas parte 4 - Subject e switchMap

--- angular/projetos/app2/src/app/topo/topo.component.ts
+++ angular/projetos/app2/src/app/topo/topo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap';
 
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
@@ -14,19 +16,30 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   private ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertaService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+      .switchMap( (termo: string) => {
+        console.log('requisição http para a api');        
+        return this.ofertaService.pesquisaOfertas(termo)
+      })
+
+    this.ofertas.subscribe( (ofertas: Oferta[]) => { console.log(ofertas); })
   }
 
-  public capturarFiltro(termoDaBusca: string) {
-     this.ofertas = this.ofertaService.pesquisaOfertas(termoDaBusca)
-     this.ofertas.subscribe( 
-        ( ofertas: Oferta[] ) => console.log(ofertas),
-        ( erro: any ) => console.log(erro.status),
-        () => console.log('Fluxo completo')
-      )
+  public pesquisa(termoDaBusca: string) {
+    //  this.ofertas = this.ofertaService.pesquisaOfertas(termoDaBusca)
+    //  this.ofertas.subscribe( 
+    //     ( ofertas: Oferta[] ) => console.log(ofertas),
+    //     ( erro: any ) => console.log(erro.status),
+    //     () => console.log('Fluxo completo')
+    //   )
+
+    console.log('keyup caracter: ', termoDaBusca);
+    this.subjectPesquisa.next(termoDaBusca)
   }
 
 }
