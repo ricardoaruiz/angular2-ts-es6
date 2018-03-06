c81604b4a32c384953bddcacc2195ad288b68ea8
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-06 15:33:26 -0300

    App2-Two-Way-Binding dos dados do formulário

--- angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string = ''
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(endereco: string): void {
+    console.log(this.endereco);    
+  }
+
+  public atualizaNumero(numero: string): void {
+    console.log(this.numero);    
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    console.log(this.complemento);    
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string): void {
+    console.log(this.formaPagamento);    
   }
 
 }
