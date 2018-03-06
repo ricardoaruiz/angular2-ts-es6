009eed79febd7bde567fbfd8048f25a96b952a30
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-06 16:15:14 -0300

    App2-Validando dados do formulário parte2 - Controlando estado primitivo dos campos

--- angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -12,10 +12,17 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = ''
   public formaPagamento: string = ''
 
+  //controles de validação dos campos
   public enderecoValido: boolean
   public numeroValido: boolean
   public complementoValido: boolean
   public formaPagamentoValido: boolean
+
+  //estados primitivos dos campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
 
   constructor() { }
 
@@ -24,21 +31,25 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco    
+    this.enderecoEstadoPrimitivo = false
     this.endereco.length > 3 ? this.enderecoValido = true : this.enderecoValido = false
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero
+    this.numeroEstadoPrimitivo = false
     this.numeroValido = this.numero.length > 0 ? true : false;
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento 
+    this.complementoEstadoPrimitivo = false
     this.complementoValido = this.complemento.length > 0 ? true : false
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento
+    this.formaPagamentoEstadoPrimitivo = false
     this.formaPagamentoValido = this.formaPagamento !== '' ? true : false
   }
 
