5cfc8540b86e9e86fefbbfb6f668eb19b61034b0
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-07 11:15:26 -0300

    App2-Enviando formulário (ngSubmit) e explorando os dados do objeto NgForm

--- angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -14,5 +16,9 @@ export class OrdemCompraComponent implements OnInit {
 
   ngOnInit() {
     
+  }
+
+  public confirmarCompra(formularioPedido: NgForm): void {
+    console.log(formularioPedido.value);    
   }
 }
