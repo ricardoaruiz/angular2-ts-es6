3f55181c7b1a24a7714f300c75a92807ec1bcf49
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-07 09:07:42 -0300

    App2-Injetando o serviço HTTP do Angular em OrdemCompraService

--- angular/projetos/app2/src/app/ordem-compra.service.ts
+++ angular/projetos/app2/src/app/ordem-compra.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 import { Pedido } from './shared/pedido.model';
+
+//import { environment } from '../environments/environment';
 
 @Injectable()
 export class OrdemCompraService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   public efetivarCompra(pedido: Pedido): void {
     console.log(pedido);
+
+    // this.http.post(`${environment.baseURL}\pedido`, pedido)
+    //   .map( (resposta: Response) => {
+    //     pedido = resposta.json();
+    //   })
     
   }
 
