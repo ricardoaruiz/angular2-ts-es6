d29c80abaad9e16d23eb95e8d96bc299d8798e6f
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-08 09:48:46 -0300

    App2 - Estrategia de implementação do carrinho de compra

--- angular/projetos/app2/src/app/app.module.ts
+++ angular/projetos/app2/src/app/app.module.ts
@@ -16,14 +16,14 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
+import { OrderCompraSucessoComponent } from './order-compra-sucesso/order-compra-sucesso.component';
 
 import { DescricaoReduzidaPipe } from './util/descricao-reduzida.pipe';
 
 //Utilizado para a Internacionalização
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
-import { OrderCompraSucessoComponent } from './order-compra-sucesso/order-compra-sucesso.component';
 registerLocaleData(localePt);
 
 @NgModule({
