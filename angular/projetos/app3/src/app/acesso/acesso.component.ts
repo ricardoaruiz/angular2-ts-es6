8a10a8893921afd131071a84e8d723fe6b76d694
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-12 15:49:40 -0300

    App3-Estado especial void e estilos na transição de estados - Login

--- angular/projetos/app3/src/app/acesso/acesso.component.ts
+++ angular/projetos/app3/src/app/acesso/acesso.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
+/*Cria uma animação na criação do componente void => criado
+deslizando o componente da esquerda para direita e vice-versa*/
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
@@ -13,16 +15,32 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition('void => criado', [
         style({
           "opacity": 0,
-          "transform": 'translate(-100px, 0)'
+          "transform": 'translate(-120px, 0)'
         }),
         animate('1000ms 0s ease-in-out')
       ])
-    ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado',style({
+        "opacity": 1
+      })),
+      transition('void => criado', [
+        style({
+          "opacity": 0,
+          "transform": 'translate(120px, 0)'
+        }),
+        animate('1000ms 0s ease-in-out')
+      ])
+    ])    
   ]
 })
 export class AcessoComponent implements OnInit {
 
+  // Estado para o banner
   public estadoBanner:string = 'criado';
+
+  // Estado para o painel de login
+  public estadoPainel: string = 'criado';
 
   constructor() { }
 
