98c520e8655cbb0cbce54322c1448b24cfb956a7
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-14 09:01:54 -0300

    App3-Cadastrando usuários no Firebase parte 2

--- angular/projetos/app3/src/app/acesso/service/autenticacao.service.ts
+++ angular/projetos/app3/src/app/acesso/service/autenticacao.service.ts
@@ -14,10 +14,23 @@ export class AutenticacaoService {
     //Criando um usuário no Authentication do Firebase
     firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
       .then( (resposta: any) => {
-        console.log(resposta);
+
+        //remover a senha do atributo senha do objeto usuário
+        delete usuario.senha;
+
+        //path usuario_detalhe/<email do usuario>
+        //Não pode ter caracter especial nos nós do firebase
+        //então vamos criptografar o email com a funcao "btoa" (atob volta a criptografia)
+        firebase.database()
+          .ref(`usuario_detalhe/${btoa(usuario.email)}`)
+            .set(usuario)
+              .catch( (erro: Error) => {
+                console.log('Erro ao criar os dados do usuário Database', erro);                
+              })
+
       })
       .catch( (erro: Error) => {
-        console.log(erro);
+        console.log('Erro ao criar o usuário Auth', erro);
       })
   }
 
