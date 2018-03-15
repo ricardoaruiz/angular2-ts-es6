e6fd7b403e216d1c741ca0fb4733bca42c1909e7
Ricardo Ruiz <ralmendro@ciandt.com>
2018-03-15 13:14:11 -0300

    App3-Controlando o progresso do upload

--- angular/projetos/app3/src/app/service/bd.service.ts
+++ angular/projetos/app3/src/app/service/bd.service.ts
@@ -18,7 +18,24 @@ export class BdService {
     firebase.storage()
       .ref()
         .child(`imagens/${nomeImagem}`)
-          .put(publicacao.imagem);
+          .put(publicacao.imagem)
+            // Método on para ficar escutando um evento do firebase
+            //nesse caso estamos escutando o evento "firebase.storage.TaskEvent.STATE_CHANGED"
+            .on(firebase.storage.TaskEvent.STATE_CHANGED,
+              //acompanhamento do progresso do upload
+              ( snapshot: any ) => {
+                console.log('Acompanhamento do upload:', snapshot);
+              },
+              //tratamento de erro
+              ( erro: Error) => {
+                console.log(erro)
+              },
+              //executado na finalização do upload
+              () => {
+                console.log('Upload completo');                
+              }
+            )
+          
 
     // Utilizamos a função btoa para transformar o email do usuário para a base64
     //pois os nós do firebase não aceitam caracteres especiais exemplo: @
